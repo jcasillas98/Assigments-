bd951fb0d67219d9a2ae990c6d14d44fc5e15ff6
jcasillas98 <72593690+jcasillas98@users.noreply.github.com>
2020-10-21 23:19:49 -0500

    Add files via upload

---
+++ Module_1_lab_activity.py
@@ -0,0 +1,59 @@
+#Module 1 Lab Activity
+#Julian Casillas
+#9/29/2020
+#To Do:
+#At the top of this file:
+#Add appropriate comments for your name, date, program description, etc.
+#
+#For each line of code below:
+#   write a comment above it describing what the expression evaluates to and why.
+#   You may use the shell to help you and check your work.
+#Follow the appropriate style from my examples and the web article
+#
+#Submit your modified version of this file to d2l by the due date.
+
+
+#Examples:
+
+#This line evaluates to 90 because subtracting 10 from 100 gives you 90.
+100 - 10
+print(100 - 10)
+
+#This line evaluates to true because 100 is greater than 10.
+100 > 10
+print(100 > 10)
+#Worth 10 points each:
+
+#This line evaluates to 4 because adding 2 from 2 gives you 4.
+2+2
+print(2 + 2)
+#This line evaluates to 3.0 because dividing 12 from 4 gives you 3 with the added decimal point.
+12/4
+print(12/4)
+#This line evaluates to 0 because there is no remainder when you divide 8 and 2. 
+8%2
+print(8%2)
+#This line evaluates to 694 because 100 is multipled by 7 which equals to 700 and then its subtracted by 4.
+100*7-4
+print(100*7-4)
+#This line is equal to 300 because it follows the order of operations, so 7 minus 4 equals 3, times 100.
+100*(7-4)
+print(100*(7-4))
+#This line is equal to 5 because it follows the order of operations, so 8 + 9 = 17, and the remainder of 80 divided by 17 is 12, so 12 - 7 equals 5.
+80%(8+9)-7
+print(80%(8+9)-7)
+#This line evaluates to true because 10 is breater than 7
+10 > 7
+print(10 > 7)
+#This line evaluates to true because 20 is greater than OR equal to 20
+20 <= 20
+print(20 <= 20)
+#This line evaluates to false because both of these expressions are not true. AND requires both expressions to be true.
+20 != 30 and 10 == 7
+print(20 != 30 and 10 == 7)
+
+#This line evalautes to true because at least one of the two expressions are true, which is what OR needs for it to be true. 
+100 > 98+2 or 7%2 == 1
+print(100 > 98+2 or 7%2 == 1)
+#BONUS: Modify this code to display the result of each line to the user through the console when this file is run
+#       (there are several ways to do this).
